ffc5122a2b78152a1454af04bd923ab0149dd318
Gerahm <gerahme.hambunan@gmail.com>
2025-03-30 16:00:32 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.push("/dashboard");
-  }, [router]);
+  // useEffect(() => {
+  //   router.push("/dashboard");
+  // }, [router]);
 
   return (
     <div className="flex items-center justify-center min-h-screen">
